fb5ca3036ea37dd135e1cc44cefa860dc36bd122
a6ee6 <a6ee6@A6EE6>
2025-02-21 11:09:01 +0000

    save

--- AzureTutorials.AppService/Program.cs
+++ AzureTutorials.AppService/Program.cs
@@ -14,6 +14,8 @@ namespace AzureTutorials.AppService
             // Add services to the container.
             builder.Services.AddAuthorization();
 
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
